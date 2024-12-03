5eda792c3866e674dedaff622668d9e89e3b8201
Theo Brigitte <theo.brigitte@gmail.com>
2024-12-03 21:37:57 +0100

    script: add generate-pairlist.sh

---
+++ scripts/generate-pairlist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source "$SCRIPT_DIR/common.sh"
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $(basename $0) <pairlist config>
+
+This script will call test-pairlist and generate a pairlist.json file."
+  exit 1
+fi
+
+args+=(
+  --config $1
+)
+
+pairs="$($FREQTRADE_DIR/.venv/bin/freqtrade test-pairlist ${args[@]} --print-json | jq -cr .)"
+jq '.exchange.pair_whitelist='"$pairs" ./pairlist/binance-pairlist-template.json > pairlist.json
+echo "==> generated: pairlist.json"
