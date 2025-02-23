e849b51a4c567d071e481e917481c90069f94c37
Theo Brigitte <theo.brigitte@gmail.com>
2025-02-23 15:36:08 +0100

    script: add header info in backtest-browser.sh

--- scripts/backtest-browser.sh
+++ scripts/backtest-browser.sh
@@ -14,4 +14,8 @@ backtest_results_dir="$SCRIPT_DIR/../backtest_results"
 ls -t $backtest_results_dir/backtest-result-*.json | \
   grep -v ".meta.json" | \
   xargs basename -a | \
-  $FZF_BIN --preview="$JQ_BIN -r '.strategy_comparison' $backtest_results_dir/{}" --bind "enter:execute($freqtrade_cmd $backtest_results_dir/{} | less +G)" --bind alt-enter:accept
+  $FZF_BIN \
+    --preview="$JQ_BIN -r '[.strategy[]|(.exit_reason_summary[]|select(.key == \"TOTAL\"))+{\"key\":.strategy_name}+pick(.expectancy,.expectancy_ratio,.profit_factor,.max_relative_drawdown)]' $backtest_results_dir/{}" \
+    --bind "enter:execute($freqtrade_cmd $backtest_results_dir/{} | less +G)" \
+    --bind alt-enter:accept \
+    --header="enter:show details, alt-enter:select entry and exit"
